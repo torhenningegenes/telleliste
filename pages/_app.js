439a610cf2ca2d0bff55e7a84357c0833a2b47f0
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-05-25 17:25:14 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Fragment from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Component {...pageProps} />;
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
