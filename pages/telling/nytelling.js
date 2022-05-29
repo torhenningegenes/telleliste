39f6aa4115a7abf3f13e6aa158b5c9900dece88c
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-05-29 12:24:11 +0200

    flytter logikk rundt kanpper

--- pages/telling/nytelling.js
+++ pages/telling/nytelling.js
@@ -1,9 +1,16 @@
 import { Fragment } from "react";
 import Barn from "./barn";
 import classes from "./nytelling.module.css";
+import { useState, useRef } from "react";
 
 function NyTelling(props) {
-  const barn = [
+  let btnCss = classes.customBtn;
+  // Setting useState for holding the state for the button presses.
+  let [counted, setCounted] = useState(false);
+  const [btnColor, setBtnColor] = useState("");
+
+  // dummy-data for kids.
+  const [barn, setBarn] = useState([
     { id: 1, navn: "Aria", tilstede: true },
     { id: 2, navn: "Per", tilstede: true },
     { id: 3, navn: "Olav", tilstede: true },
@@ -17,14 +24,18 @@ function NyTelling(props) {
     { id: 11, navn: "Nora", tilstede: true },
     { id: 12, navn: "Tor Henning", tilstede: true },
     { id: 13, navn: "Heidi", tilstede: true },
-  ];
-
-  function counted() {
-    console.log("Click");
-
-    return;
-    //Remove from list. use state
+  ]);
+  let barnIdClicked;
+  function barnCounted() {
+    setCounted((current) => !current);
+    console.log(counted);
+    // barn id clicked.
+    let barnId;
+    for (const b of barn) {
+      console.log(b.id);
+    }
   }
+
   return (
     <Fragment>
       <div className="container ">
@@ -33,10 +44,14 @@ function NyTelling(props) {
             <li className="list-group-item list-barn" key={b.id}>
               <Barn navn={b.navn} />
               <button
-                className="btn btn-lg  btn-success rounded-pill"
-                onClick={counted}
+                id="countBtn"
+                className={`${btnCss}`}
+                onClick={barnCounted}
+                style={{
+                  backgroundColor: counted ? "salmon" : "",
+                }}
               >
-                Ok
+                ✓
               </button>
             </li>
           ))}
