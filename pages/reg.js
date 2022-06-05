552abb71e534a00306a6016c03033c5d71784713
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-06-05 22:32:14 +0200

    worked on counting logic, and context for the app

--- pages/reg.js
+++ pages/reg.js
@@ -1,10 +1,4 @@
 function Registrering() {
-  const test = "test";
-
-  let date = new Date();
-  date = date.toLocaleDateString();
-  console.log(date);
-  console.log("test");
   return (
     <div>
       <h1>Registrering</h1>
