54ce5f6539965fa9213b67dd52cdf80138c83bbc
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-06-12 08:18:16 +0200

    more work on auth

--- context/auth-context.js
+++ context/auth-context.js
@@ -1,5 +1,9 @@
 import React, { useContext, useState } from "react";
-import { auth } from "../firebase/initFirebase";
+import { getAuth } from "../firebase/initFirebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const AuthContext = createContext();
 
@@ -8,6 +12,22 @@ export function useAuth() {
 }
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
+
+  function signUp() {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
+
   const value = {
     currentUser,
   };
