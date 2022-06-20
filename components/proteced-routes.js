f040f960c334b086be436751418cabcd05949c2b
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-06-20 15:37:59 +0200

    added protected routes

---
+++ components/proteced-routes.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import { useAuth } from "../context/auth-context";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+const ProtecedRoutes = ({ children }) => {
+  const { currentUser } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!currentUser) {
+      router.push("login-user");
+    }
+  }, [router, currentUser]);
+  return <Fragment>{currentUser ? children : null}</Fragment>;
+};
+
+export default ProtecedRoutes;
