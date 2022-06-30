f299821ae0ca14eb8ba09d6a525bf51be36b7cdd
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-06-30 23:13:33 +0200

    some work on the read from db component

--- firebase/firestore-read.js
+++ firebase/firestore-read.js
@@ -5,7 +5,7 @@ import { db } from "./firebaseConfig";
 export function FirestoreRead() {
   const readFromDB = async function () {
     const querySnapshot = await getDocs(collection(db, "users"));
-    const docdata = querySnapshot;
+    const docdata = querySnapshot.data();
     console.log(JSON.stringify(docdata));
     querySnapshot.forEach((doc) => {
       console.log(`${doc.id} => ${doc.data()}`);
