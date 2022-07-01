9a03ff526dc582e5ee36e978710aec915a4a205d
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-07-01 14:46:49 +0200

    working on writing to db

--- firebase/firestore-write.js
+++ firebase/firestore-write.js
@@ -1,42 +1,55 @@
 import { db } from "./firebaseConfig";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, setDoc } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
 import { Button } from "react-bootstrap";
-
+const citiesRef = collection(db, "cities");
 export function FirestoreWrite() {
   const addToDB = async function () {
-    try {
-      const docRef = await addDoc(collection(db, "users"), {
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815,
-      });
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  };
-  const writeToDB = async function () {
-    try {
-      const docRef = await addDoc(collection(db, "testers"), {
-        first: "Alan",
-        middle: "Mathison",
-        last: "Turing",
-        born: 1912,
-      });
-
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
+    await setDoc(doc(citiesRef, "SF"), {
+      name: "San Francisco",
+      state: "CA",
+      country: "USA",
+      capital: false,
+      population: 860000,
+      regions: ["west_coast", "norcal"],
+    });
+    await setDoc(doc(citiesRef, "LA"), {
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA",
+      capital: false,
+      population: 3900000,
+      regions: ["west_coast", "socal"],
+    });
+    await setDoc(doc(citiesRef, "DC"), {
+      name: "Washington, D.C.",
+      state: null,
+      country: "USA",
+      capital: true,
+      population: 680000,
+      regions: ["east_coast"],
+    });
+    await setDoc(doc(citiesRef, "TOK"), {
+      name: "Tokyo",
+      state: null,
+      country: "Japan",
+      capital: true,
+      population: 9000000,
+      regions: ["kanto", "honshu"],
+    });
+    await setDoc(doc(citiesRef, "BJ"), {
+      name: "Beijing",
+      state: null,
+      country: "China",
+      capital: true,
+      population: 21500000,
+      regions: ["jingjinji", "hebei"],
+    });
   };
   return (
     <>
       <Button variant="primary" onClick={addToDB}>
         Write to DB 1
-      </Button>
-      <Button variant="primary" onClick={writeToDB}>
-        Write to DB 2
       </Button>
     </>
   );
