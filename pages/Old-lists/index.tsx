4241f518d185a8fdba52242eed244ff5ba3352a1
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-07-12 21:27:46 +0200

    getting closer to solve filter logic

--- pages/Old-lists/index.tsx
+++ pages/Old-lists/index.tsx
@@ -3,7 +3,7 @@ import uniqid from "uniqid";
 import Button from "../../node_modules/react-bootstrap/esm/Button";
 import { getDatabase, ref, onValue, get, child } from "firebase/database";
 import List from "./list";
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import SearchBar from "./search-bar";
 import Container from "../../node_modules/react-bootstrap/esm/Container";
 
@@ -13,34 +13,43 @@ import Container from "../../node_modules/react-bootstrap/esm/Container";
 
 function OldLists() {
   const [showOldLists, setShowOldLists] = useState(false);
+  const [oldLists, setOldLists] = useState();
+  let dbRef = useRef([]);
 
   // send a call to the database
   let avdeling: string;
   let navn: string;
   let dbData: any[];
 
-  const readFromDB = async function () {
-    const db = getDatabase();
-    const tellingerPath = ref(db, "Tellinger");
+  //const readFromDB = async function () {
+  const db = getDatabase();
+  const tellingerPath = ref(db, "Tellinger");
 
+  useEffect(() => {
     onValue(tellingerPath, (snapshot) => {
       const data = snapshot.val();
       console.log(data);
-      dbData = data;
-      //console.log(dbData);
+      if (data !== null) {
+        dbRef.current = [...Object.values(data)];
+
+        //console.log("Hei fra useRef", dbRef.current);
+      }
     });
+  }),
+    [];
 
-    // const newArr = Object.values(dbData);
-    // const returnArr = Object.entries(newArr);
-    // console.log(returnArr);
-  };
-  readFromDB();
+  // const newArr = Object.values(dbData);
+  // const returnArr = Object.entries(newArr);
+  // console.log(returnArr);
+  //};
+
+  //readFromDB();
+  //console.log(oldLists);
   // Map out old lists from DB
-  const renderList = function () {
-    let newArr = dbData;
-    console.log(newArr);
-    return newArr;
 
+  console.log(dbRef.current);
+  const renderList = function () {
+    setShowOldLists(true);
     // flatArr.filter((e) => {
     //   if (e.avdeling) {
     //     console.log(e);
@@ -53,8 +62,6 @@ function OldLists() {
     // });
   };
 
-  let test = renderList();
-  console.log(test);
   return (
     <div className="margin--top--medium bg-light">
       <Container>
@@ -63,7 +70,7 @@ function OldLists() {
           <Button onClick={renderList}>Console.log liste</Button>
         </div>
         <SearchBar />
-        <h1>{test[2]}</h1>
+        <h1>her blir det liv rai rai</h1>
       </Container>
     </div>
   );
