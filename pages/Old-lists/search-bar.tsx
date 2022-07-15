87640fe34972bb7f0dd3aae8594915134920e730
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-07-15 21:16:12 +0200

    workwork

--- pages/Old-lists/search-bar.tsx
+++ pages/Old-lists/search-bar.tsx
@@ -3,11 +3,16 @@ import { Form, Button, Container } from "react-bootstrap";
 
 function SearchBar(props) {
   const { formState, setFormState } = props;
-  let formRef: string = useRef("Søk her");
+  let formRef: string = useRef("");
 
   // Take value from search. Use string methods like make lower case and remove whitespaces. Then filter on word `.filter(${input})`
   // If ${input} === true render html with list.
 
+  const handleSubmit = (e) => {
+    console.log("Click");
+    e.preventDefault();
+    console.log(formRef);
+  };
   return (
     <Fragment>
       <Container
@@ -18,15 +23,15 @@ function SearchBar(props) {
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Søk i gamle lister</Form.Label>
             <Form.Control
-              type="search"
+              onSubmit={handleSubmit}
+              type="text-box"
               placeholder="Søk her"
-              value={formState}
-              onChange={(e) => setFormState(e.target.value)}
+              refer={formRef}
             />
             {/* <Form.Text className="text-muted">Søk</Form.Text> */}
           </Form.Group>
 
-          <Button variant="primary" type="submit" className="rounded btn-lg">
+          <Button variant="primary" type="submit" className="rounded btn-lg ">
             Søk etter gamle lister
           </Button>
         </Form>
