59e18980c515ead78ac8be501ca4e308c03425f0
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-08-02 20:59:31 +0200

    slight progress on tailwind navbar

--- pages/home.js
+++ pages/home.js
@@ -1,15 +1,23 @@
 import Link from "next/link";
-
+import { useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import { useAuth } from "../context/auth-context";
 
 function Home(props) {
   const { currentUser } = useAuth();
+  const [showMenu, setShowMenu] = useState(false);
+  const showHamburgerMenuHandler = function () {
+    setShowMenu(true);
+    console.log("click");
+    if (showMenu === true) {
+      setShowMenu(false);
+    }
+  };
   return (
     <>
-      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 mt-6">
+      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 mt-11">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
-          <a href="https://flowbite.com/" class="flex items-center">
+          <a href="https://flowbite.com/" className="flex items-center">
             <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="mr-3 h-6 sm:h-9"
@@ -20,6 +28,7 @@ function Home(props) {
             </span>
           </a>
           <button
+            onClick={showHamburgerMenuHandler}
             data-collapse-toggle="navbar-default"
             type="button"
             className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -35,21 +44,28 @@ function Home(props) {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <div
+            className={
+              showMenu
+                ? "w-full md:block md:w-auto"
+                : "hidden w-full md:block md:w-auto"
+            }
+            id="navbar-default"
+          >
+            <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ">
               <li>
                 <a
                   href="#"
-                  className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                  className="block py-2 pr-4 pl-3 text-white bg-sky-800 rounded md:bg-transparent md:text-sky-500 md:p-0 dark:text-white"
                   aria-current="page"
                 >
-                  Home
+                  Hjem
                 </a>
               </li>
               <li>
@@ -57,7 +73,7 @@ function Home(props) {
                   href="#"
                   className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                  About
+                  Ny telling
                 </a>
               </li>
               <li>
@@ -65,7 +81,7 @@ function Home(props) {
                   href="#"
                   className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                  Services
+                  Gamle lister
                 </a>
               </li>
               <li>
@@ -73,15 +89,7 @@ function Home(props) {
                   href="#"
                   className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Contact
+                  Logg ut
                 </a>
               </li>
             </ul>
