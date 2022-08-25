b9a6e0deb930d7098b52793515424bbba66cf183
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-08-25 07:03:06 +0200

    changed the text size of current date

--- components/current-date.js
+++ components/current-date.js
@@ -27,7 +27,7 @@ function CurrentDate() {
   // console.log(currentDate);
   // const dayAndDate = currentDay + " " + currentDate;
   return (
-    <h5 className="ms-2 text-gray-700 uppercase leading-3 mt-4 mb-4 text-sm md:text-base">
+    <h5 className="ms-2 text-gray-700 uppercase leading-3 mt-4 mb-4 text-sm md:text-xl">
       {currentDate}
     </h5>
   );
