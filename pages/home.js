418789c1786122838b53c4d380eee3a373fed04c
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-09-02 14:43:01 +0200

    changed buttons on home screen

--- pages/home.js
+++ pages/home.js
@@ -6,25 +6,26 @@ import wave from "../img/Wave.svg";
 function Home(props) {
   const { currentUser } = useAuth();
   console.log(currentUser);
-  //Adder en kommentar bare for å sjekke
+
   return (
     <>
-      <section className=" text-center bg-gray-50 p-3 shadow rounded-1 mx-auto h-screen min-w-min z-0">
+      <section className=" text-center bg-gray-50 p-3 shadow rounded-1 mx-auto h-screen min-w-min z-0 flex flex-col">
         <h1 className="text-gray-600 uppercase tracking-wide z-10">
           Velkommen til tellelista!
         </h1>
         {/* <h1>{currentUser.email}</h1> */}
-
-        <Link href={"/Reg"} passHref className="block z-20">
-          <button className="bg-sky-800 hover:bg-sky-500 text-sky-100 font-light py-2 px-4 rounded focus:ring-red-900 focus:shadow-outline active:bg-red-500 mr-3">
-            Ny telling
-          </button>
-        </Link>
-        <Link href={"/Old-lists"} passHref className="block z-20">
-          <button className="bg-sky-800 text-sky-100 hover:bg-sky-500 font-light py-2 px-4 rounded focus:ring-red-900 focus:shadow-outline active:bg-red-500">
-            Se gamlelister
-          </button>
-        </Link>
+        <div className="flex flex-col max-h-44 justify-between align-middle items-center mt-6">
+          <Link href={"/Reg"} passHref className="block z-20">
+            <button className="bg-sky-800 hover:bg-sky-500 text-sky-100 font-light py-4 px-4  focus:ring-red-900 focus:shadow-outline active:bg-red-500  rounded-full mb-4">
+              Ny telling
+            </button>
+          </Link>
+          <Link href={"/Old-lists"} passHref className="block z-20">
+            <button className="bg-sky-800 text-sky-100 hover:bg-sky-500 font-light py-4 px-4  focus:ring-red-900 focus:shadow-outline active:bg-red-500 rounded-full">
+              Se gamlelister
+            </button>
+          </Link>
+        </div>
       </section>
       {/* 
       <div className="z-10">
